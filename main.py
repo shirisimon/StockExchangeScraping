df2a7956f4b9dc69aad6116d91ecaebbf25b304b
shirisimon <shirisimon85@gmail.com>
2016-06-23 19:18:26 +0300

    send telegram notification func

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import splinter
 import pickle
 import time
-import smtplib
-from email.mime.text import MIMEText
+import telegram
 import numpy as np
 
 def visit_stock_webpage(stock_num):
@@ -80,23 +79,15 @@ def prep_report(stocks_mapping, prices_changes, exceed_thresh):
     """
     txt = open('temp', 'r+')
     for k,v in stocks_mapping.iteritems():
-        line = "{:<10} : {:05.2f} | {}".format(k, prices_changes[str(v)], exceed_thresh[str(v)])
+        line = "{:<10} : {:05.2f} | {} \n".format(k, prices_changes[str(v)], exceed_thresh[str(v)])
         txt.write(line)
     return txt
 
-def send_notification(report, emails):
-    " send change report in email"
-    msg = MIMEText(report.read())
-    report.close()
-    server = smtplib.SMTP('gmail-smtp-in.l.google.com:25')
-    server.starttls()
-    # server.login('username','password')
-    server.sendmail(emails['from'], emails['to'], msg.as_string())
-    server.quit()
+def send_telegram_notification(report, token, chat):
+    bot = telegram.Bot(token)
+    bot.send_message(chat_id=chat, text=report)
+    return
 
-# TODO: without login
-# http://stackoverflow.com/questions/24270715/send-anonymous-mail-from-local-pc
-# http://stackoverflow.com/questions/9763455/how-to-send-a-mail-directly-to-smtp-server-without-authentication
 
 # params:
 baseline_date     = '12062016'  # baseline date for peaking prices
@@ -104,7 +95,8 @@ threshold         = .25         # percentage change in price
 save_baseline     = False        # True - get baseline data and save it, False - load previous
 notification      = 'all'       # 'all' = whether exceeds thresh, and value changes, 'thresh' - only if exceed thresh, 'value_change'
 notification_freq = 'weekly'
-emails            = {'from': 'shirisimon85@gmail.com', 'to': 'shirisimon85@gmail.com'}
+telegram_token    = '199306554:AAF_NAQdNbOVL9Ac3nyHJ6cwbR3HkM66eRM'
+telegram_chatid   = '72905054'
 
 # load stocks_data
 stocks_mapping = pickle.load(open("stocks_mapping.p", "rb"))
@@ -120,11 +112,25 @@ current_prices = baseline_prices # get_all_stock_prices(stock_list)
 exceed_thresh, prices_changes = check_changes(baseline_prices, current_prices, threshold)
 # prep and send report
 report = prep_report(stocks_mapping, prices_changes, exceed_thresh)
-send_notification(report, emails)
+send_telegram_notification(report, telegram_token, telegram_chatid)
 
 
 # TODO:
 # add send notification function and debug
 # add gold
-# check how to run frequently and from where, how to set the frequency check parameter...
 # make a website to publish
+
+# TODO: run daily
+# import schedule
+# import time
+#
+# def job():
+#     print("I'm working...")
+#
+# schedule.every(10).minutes.do(job)
+# schedule.every().hour.do(job)
+# schedule.every().day.at("10:30").do(job)
+#
+# while 1:
+#     schedule.run_pending()
+#     time.sleep(1)
